4304fc189d1f071af5a6e09ca1f6530c6f950a3d
吳宗融 <bc781208@hotmail.com>
2020-05-04 16:37:49 +0800

    戰團準備

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_COOKIE['LOG'])){
+    // switch($_COOKIE['LOG']){    
+    // };
+}else{
+
+};
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>戰團</title>
+</head>
+<body>
+  <h1>歡迎加入</h1>  
+</body>
+</html>
---
+++ warteam/dbconnect.php
@@ -0,0 +1,6 @@
+<?php
+$dns="mysql:host=localhost;charset=utf8;dbname=warorder";
+$pdo=new PDO($dns,"root","");
+date_default_timezone_set("Asia/Taipei");
+
+?>
